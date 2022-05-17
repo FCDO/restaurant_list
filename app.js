8189905114739e18e776021184c0686b0d1a5acf
YiHong <english111026@gmail.com>
2022-05-17 16:14:31 +0800

    feat add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 //require package used in the project
-
+const bodyparser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
 const restaurant_list = require('./restaurant.json')
 
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.use(bodyparser.urlencoded({ extended: true }))
 //require express-handlebars here
 const exphbs = require('express-handlebars')
 
