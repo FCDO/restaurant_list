162cfc78145eea8cd71de7bf62f9d9eb61b7d756
YiHong <english111026@gmail.com>
2022-05-31 17:23:52 +0800

    feat:add bcrypt to login route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const User = require('../../models/user')
 const router = express.Router()
 
@@ -37,7 +38,7 @@ router.post('/register', (req, res) => {
   User.findOne({ email }).then(user => {
     if (user) {
       errors.push({ message: '這個 Email 已經註冊過了。' })
-      console.log('User already exists.')
+
       res.render('register', {
         errors,
         name,
@@ -45,15 +46,19 @@ router.post('/register', (req, res) => {
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-    }
+        password: hash
+      }))
+
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+
   })
     .catch(error => console.log(error))
 })
