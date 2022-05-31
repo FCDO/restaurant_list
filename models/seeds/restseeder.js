d185ae96702346a328d0e1bcd958bee517bf7a62
YiHong <english111026@gmail.com>
2022-06-01 00:02:21 +0800

    modify restseeder.js

--- models/seeds/restseeder.js
+++ models/seeds/restseeder.js
@@ -1,17 +1,63 @@
-
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const restList = require('../restaurant')
+const User = require('../user')
 const restaurant_list = require('./restaurant.json')
 const db = require('../../config/mongoose')
 
+const SEED_USER = [
+  {
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurant: [1, 2, 3]
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurant: [4, 5, 6]
+  }
+]
 
 
 db.once('open', () => {
+  return Promise.all(
+    Array.from(
+      { length: SEED_USER.length },
+      (value, i) => {
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
+          .then(hash => User.create({
+            email: SEED_USER[i].email,
+            password: hash
 
-  for (let i = 0; i < restaurant_list.results.length; i++) {
-    restList.create(restaurant_list.results[i])
+          }))
+          .then(user => {
+            const myRestaurants = restaurant_list.results.filter(element => {
+              return SEED_USER[i].restaurant.includes(element.id)
+            })
+            const userId = user._id
 
-  }
-  console.log('done')
+            return Promise.all(
+              Array.from(
+                myRestaurants,
+                (value) => {
+                  value.userId = userId
+                  return restList.create(value)
+                }
+              )
+            )
+          })
+          .then(() => {
+            console.log('done.')
+            process.exit()
+          })
+      }
+    )
+  )
+
 
 })
 
